7bea086061dbbce736db764a7a90d673fee2c95d
administrator <administrator@administrators-MacBook-Pro.local>
2018-08-29 15:16:05 -0700

    Fix bugs in JS code and delete the bootstrap button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,40 +4,47 @@ var arr = ["start","ok","dark","dock","fake","quick","world","bucks"];
 var pressedKey = [];
 // Random Pick one word and turn string to array
 var wordRandom = arr[Math.floor(Math.random() * arr.length)].split("");
+var oriArr = Array(wordRandom.length).fill("*")
+var oriStr = oriArr.join("");
 //! Cheating Code
 console.log(wordRandom);
 
-// Data Process Module
-var oriArr = Array(wordRandom.length).fill("*")
-var oriStr = oriArr.join("");
-//Character flip function & disable used key & reset or a trigger("gameStart()") 
-function charFlip (char){
-        var n = wordRandom.indexOf(char)
-        while (n !== -1){
-            oriArr[n] = wordRandom[n];
-            n = wordRandom.indexOf(char,n+1);
-        }
-//*    console.log("*** change to " + oriArr);
-//*    console.log(userInput);
-        document.getElementById(userInput.toUpperCase()).disabled = true;
-        gameStart();  
-}
 //Loop Control Module 
 var i = 0;
 function gameStart(){
-    document.getElementById("passwordInput").value = oriArr.join(""); 
+    
     alert("Input Character");
     i++;
-// Define how many time user can try basing on the random word length
-//TODO Modify the rate number for Increase the difficulty.
-    if (i >= Math.ceil(wordRandom.length * 1.5) ){
-    alert("Times UP");
-    };
-    if (oriArr.join("") === wordRandom.join("") ){
-        alert("what a great job")
-    }; 
+    // Define how many time user can try basing on the random word length
+    //TODO Modify the rate number for Increase the difficulty.
+    
 }
 
+// Data Process Module
+function charFlip (char){
+    var n = wordRandom.indexOf(char)
+    while (n !== -1){
+        oriArr[n] = wordRandom[n];
+        n = wordRandom.indexOf(char,n+1);
+    }
+    // console.log("*** change to " + oriArr);
+    // console.log(userInput);
+    // console.log(i);
+    // console.log(oriArr.join(""));
+    // console.log(wordRandom.join(""));
+    // Define how many time user can try basing on the random word length
+    //TODO Modify the rate number for Increase the difficulty.
+    document.getElementById(userInput.toUpperCase()).disabled = true;
+    document.getElementById("passwordInput").value = oriArr.join(""); 
+    if (i < Math.ceil(wordRandom.length * 1.5) && oriArr.join("") != wordRandom.join("") ){
+        gameStart();  
+    } else if ( i <= Math.ceil(wordRandom.length * 1.5) && oriArr.join("") === wordRandom.join("")  ) {
+        var j = 999;
+        console.log(j);
+    } else {
+        alert("I know, you tried!")
+    }
+}
 
 
 
